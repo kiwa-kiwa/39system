b5ba96be1323cab48d853cd808ff2597359a98ef
sonangrai <shonahangrae@gmail.com>
2020-11-10 11:17:20 +0545

    added the table toggles

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readXlsxFile = require("read-excel-file/node");
+var mysql = require("mysql");
 const input = document.getElementById("ec");
 const input2 = document.getElementById("vc");
 
@@ -6,18 +7,12 @@ input.addEventListener("change", () => {
   document.getElementById("result").style.display = "block";
   readXlsxFile(input.files[0].path).then((rows) => {
     // rows.forEach(createTable(rows));
-    createTable(rows);
-
-    const columnNames = rows.shift(); // Separate first row with column names
-    const objs = rows.map((row) => {
-      // Map the rest of the rows into objects
-      const obj = {}; // Create object literal for current row
-      row.forEach((cell, i) => {
-        obj[columnNames[i]] = cell; // Use index from current cell to get column name, add current cell to new object
-      });
-      return obj;
-    });
-    console.log(objs);
+    if (document.getElementById("tbl-part") == null) {
+      createTable(rows);
+    } else {
+      document.getElementById("tbl-part").remove();
+      createTable(rows);
+    }
   });
 });
 
@@ -32,6 +27,7 @@ input2.addEventListener("change", () => {
 function createTable(tableData) {
   var container = document.createElement("section");
   container.classList.add("table-container");
+  container.id = "tbl-part";
   var table = document.createElement("table");
   table.id = "tbl";
   var tableBody = document.createElement("tbody");
@@ -57,3 +53,26 @@ const btnclck = document.getElementById("find");
 btnclck.addEventListener("click", () => {
   alert("We will add to db");
 });
+
+//mysql connection setup
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: "3306",
+  user: "root",
+  password: "",
+  database: "db_aggregator",
+  multipleStatements: true,
+});
+//Saving To DB
+function savedb(row) {
+  var responseJson = JSON.stringify(row);
+
+  var query = connection.query(
+    `INSERT INTO test (FirstName, LastName, Gender, Country, Age, Date, Id) VALUES  (${responseJson})`,
+    function (err, result) {
+      if (err) throw err;
+      console.log("data inserted");
+    }
+  );
+  console.log(query.sql);
+}
